2095e3c57b910f29c1adb6eb42ce4a904367157d
Elena Mitrofanova <137034439+Elena-Mi@users.noreply.github.com>
2024-08-29 13:31:34 +0800

    Update IndexPage.js

--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -9,7 +9,15 @@ export default function IndexPage() {
 
   useEffect( () => {
     axios.get('https://airbnv-backend.onrender.com/api/places').then(response => {
-      setPlaces(response.data)
+      setPlaces(response.data),
+         {
+                withCredentials: true,
+                
+              },
+            headers: {
+                'Content-Type': 'application/json',
+                "Access-Controll-Allow-Origin":"*"
+            }
     },[])
   })
 
